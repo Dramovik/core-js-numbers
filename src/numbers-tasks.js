106341318fca9a6c522cd2fdf7140d837b0914b8
Dramovik <dramovik1@gmail.com>
2025-09-19 19:52:02 +0300

    feat: getCountOfOddNumbers

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -656,8 +656,15 @@ function getHypotenuse(a, b) {
  * 10 => 5
  * 15 => 8
  */
-function getCountOfOddNumbers(/* number */) {
-  throw new Error('Not implemented');
+function getCountOfOddNumbers(number) {
+  const positiveNumber = Math.abs(number);
+  let count = 0;
+  for (let i = 0; i <= positiveNumber; i += 1) {
+    if (i % 2 > 0) {
+      count += 1;
+    }
+  }
+  return count;
 }
 
 module.exports = {
